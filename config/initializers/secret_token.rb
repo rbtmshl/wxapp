6615deedfd3a88d96e158d181f4e441f1b405648
Robert Marshall <robert.marshallii1@gmail.com>
2012-07-20 18:54:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wxapp::Application.config.secret_token = '0ebc4906734926b45e4b9d3c9e16a24e53465ff0fa2cfb496692ae99c69e716a2e762f5a0d13e2596959a5bf9e7be44961a1ad69820e982c09b862ea455fbc01'
