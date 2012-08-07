a724f0cd9df54440de45818c69ad9129a8d7472f
Robert Marshall <robert.marshallii1@gmail.com>
2012-08-06 17:46:00 -0700

    fuck this

--- db/migrate/20120804005831_add_ip_lat_lon_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddIpLatLonToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :ip_address, :string
-    add_column :users, :latitude, :float
-    add_column :users, :longitude, :float
-  end
-end
