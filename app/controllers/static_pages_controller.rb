dd20f87288821a1d591d02f21750d704fe1570c0
Robert Marshall <robert.marshallii1@gmail.com>
2012-09-06 06:20:36 -0700

    added current temp and conditions for user's detected location

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,15 +2,30 @@ class StaticPagesController < ApplicationController
 
   def home
     require 'open-uri'
-    doc = Nokogiri::XML(open("http://w1.weather.gov/xml/current_obs/KSEA.xml"))
-    temperature_string = doc.xpath("//temperature_string")
-    @temperature = temperature_string[0].to_s
     docname = "http://www.geoplugin.net/xml.gp?ip=" + requestion.ip
     docu = Nokogiri::XML(open(docname))
     city = docu.xpath("//geoplugin_city")
-    @city = city[0].to_s
-    state = docu.xpath("//geoplugin_region")
-    @state = state[0].to_s
+    if (city.length < 2)
+      @city = "Portland"
+      @state = "OR"
+      @lat = "45.58"
+      @lon = "-122.6"
+    else
+      @city = city[0].content
+      state = docu.xpath("//geoplugin_region")
+      @state = state[0].content
+      lat = docu.xpath("//geoplugin_latitude")
+      @lat = lat[0].content
+      lon = docu.xpath("//geoplugin_longitude")
+      @lon = lon[0].content
+    end
+
+    doc_string = "http://forecast.weather.gov/MapClick.php?lat="+ @lat +"&lon="+ @lon +"&FcstType=dwml"
+    doc = Nokogiri::XML(open(doc_string))
+    curtemp = doc.xpath('//temperature[@type = "apparent"]/value')
+    @temperature = curtemp[0].content
+    @curconditions = doc.xpath('//weather-conditions[@weather-summary]').last.values.last.to_s
+    
 
     if signed_in?
       @micropost  = current_user.microposts.build
