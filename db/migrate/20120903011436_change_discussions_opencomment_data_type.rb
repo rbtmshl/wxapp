92165b757ece61516faefb75af5b2d91327b0f8f
Robert Marshall <robert.marshallii1@gmail.com>
2012-09-29 02:09:19 -0700

    hihi

--- db/migrate/20120903011436_change_discussions_opencomment_data_type.rb
+++ db/migrate/20120903011436_change_discussions_opencomment_data_type.rb
@@ -1,11 +1,9 @@
 class ChangeDiscussionsOpencommentDataType < ActiveRecord::Migration
   def up
-    remove_index :discussions, [:forum_id, :subforum_id, :updated_at]
     change_column(:discussions, :opencomment, :text, limit: nil)
   end
 
   def down
-    add_index :discussions, [:forum_id, :subforum_id, :updated_at]
     change_column(:discussions, :opencomment, :string)
   end
 end
